72838eb1456af1fc3b7ada2ebbb13a48aad4e112
Akito Mochizuki <ak-mochi@iij.ad.jp>
2013-03-21 09:41:04 +0900

    Initial commit

---
+++ mrblib/tempfile.rb
@@ -0,0 +1,31 @@
+class Tempfile < File
+  def close(real=false)
+
+  end
+
+  def close!
+
+  end
+
+  def delete
+
+  end
+
+  alias :unlink :delete
+
+  def length
+
+  end
+
+  alias :size :length
+
+  def open
+
+  end
+
+  def path
+
+  end
+
+end
+
