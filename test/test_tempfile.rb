c20b6731f599f10bb7a5e4bd27fa44443dd33645
Akito Mochizuki <ak-mochi@iij.ad.jp>
2014-04-15 12:53:09 +0900

    add testcase, Tempfile#close(true)

--- test/test_tempfile.rb
+++ test/test_tempfile.rb
@@ -2,10 +2,17 @@ assert('Tempfile remove tempfile (when GC run)') do
   t = Tempfile.new 'test'
   path = t.path
   assert_true File.exist?(path)
-  t.close
+  t.close false
   assert_true File.exist?(path)
   t = nil
   GC.start
   assert_false File.exist?(path)
 end
 
+assert('Tempfile remove tempfile (call Tempfile#close with real = true)') do
+  t = Tempfile.new 'test'
+  path = t.path
+  assert_true File.exist?(path)
+  t.close true
+  assert_false File.exist?(path)
+end
